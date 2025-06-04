a96d1b6aff134a95f5698c9f8c3112cf558cb4cc
monofuel <monofuel@japura.net>
2025-06-04 14:43:11 -0400

    add comments to make the example more clear

--- examples/example.nim
+++ examples/example.nim
@@ -5,18 +5,28 @@ type MyComponent = ref object of WebComponent
 
 # Define our custom web element
 proc constructor(e: MyComponent) =
+    ## the constructor is called when the component is created, but not yet mounted to the DOM.
+    ## You probably don't want to do anything here as no DOM properties are available yet.
+    ## Can be used for setting up internal state or a shadow DOM.
     echo "MyComponent created"
 
 proc connectedCallback(e: MyComponent) =
+    ## the connectedCallback is called when the component is mounted to the DOM.
+    ## This is likely where you want to do your initial setup.
+    ## You can read attributes here.
     e.innerHTML = "<p>Hello from MyComponent2</p>"
 
 proc disconnectedCallback(e: MyComponent) =
+    ## the disconnectedCallback is called when the component is removed from the DOM.
+    ## This is where you clean things up.
     echo "MyComponent disconnected"
 
 proc adoptedCallback(e: MyComponent) =
+    ## the adoptedCallback is called when the component is moved to a new document or across an iframe boundary.
     echo "MyComponent adopted"
 
 proc attributeChangedCallback(e: MyComponent; name: cstring; oldValue: cstring; newValue: cstring) =
+    ## Listen for attribute changes.
     echo "MyComponent attribute changed: ", $name, " ", $oldValue, " ", $newValue
 
 # register our custom web element
@@ -25,7 +35,7 @@ setupNimponent("my-component",
   connectedCallback,
   disconnectedCallback,
   adoptedCallback,
-  @["my-attr".cstring],
+  @["my-attr".cstring], # You have to provide a list of the attributes you want to listen for changes for.
   attributeChangedCallback
 )
 
